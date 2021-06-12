2415837518088fa017f98cb3358819c30c79589d
dans77777 <61909995+dans77777@users.noreply.github.com>
2021-06-12 23:05:29 +0530

    pull data from firestore

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,8 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(itemBuilder: (ctx, i) => Text('adding chats'));
+    return Scaffold(
+        body: ListView.builder(
+          itemBuilder: (ctx, i) => Text('adding chats'),
+          itemCount: 10,
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            Firestore.instance
+                .collection('chats/KI9juBtoDD141S6oQU2A/messages')
+                .snapshots()
+                .listen((data) {
+              print(data.documents[0]['text']);
+            });
+          },
+          child: Icon(Icons.add),
+        ));
   }
 }
