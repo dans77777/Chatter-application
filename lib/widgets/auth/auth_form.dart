c077f623fbbe6c0cff0d79430c9285e9d4755729
dans77777 <61909995+dans77777@users.noreply.github.com>
2021-06-13 07:21:39 +0530

    adding form validations

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -17,13 +17,31 @@ class _AuthFormState extends State<AuthForm> {
             child: Form(
               child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
                 TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty || (!value.contains('@'))) {
+                      return 'Enter valid email';
+                    }
+                    return null;
+                  },
                   keyboardType: TextInputType.emailAddress,
                   decoration: InputDecoration(labelText: 'Email Address'),
                 ),
                 TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty || value.length < 7) {
+                      return 'passwords must be atleast 7 characters long';
+                    }
+                    return null;
+                  },
                   decoration: InputDecoration(labelText: 'UserName'),
                 ),
                 TextFormField(
+                  validator: (value) {
+                    if (value.isEmpty || value.length < 4) {
+                      return 'please enter atleast 4 characters';
+                    }
+                    return null;
+                  },
                   decoration: InputDecoration(labelText: 'Password'),
                   obscureText: true,
                 ),
